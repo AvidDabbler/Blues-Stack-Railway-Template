fc9a6444b2dbeb7815c74e9cbaea09abca9c22ec
AvidDabbler <walter.k.jenkins@gmail.com>
2023-04-08 20:01:49 -0500

    idk

--- app/db.server.ts
+++ app/db.server.ts
@@ -28,18 +28,18 @@ function getClient() {
 
   const isLocalHost = databaseUrl.hostname === "localhost";
 
-  const PRIMARY_REGION = isLocalHost ? null : process.env.PRIMARY_REGION;
-  const FLY_REGION = isLocalHost ? null : process.env.FLY_REGION;
+  // const PRIMARY_REGION = isLocalHost ? null : process.env.PRIMARY_REGION;
+  // const FLY_REGION = isLocalHost ? null : process.env.FLY_REGION;
 
-  const isReadReplicaRegion = !PRIMARY_REGION || PRIMARY_REGION === FLY_REGION;
+  // const isReadReplicaRegion = !PRIMARY_REGION || PRIMARY_REGION === FLY_REGION;
 
-  if (!isLocalHost) {
-    databaseUrl.host = `${FLY_REGION}.${databaseUrl.host}`;
-    if (!isReadReplicaRegion) {
-      // 5433 is the read-replica port
-      databaseUrl.port = "5433";
-    }
-  }
+  // if (!isLocalHost) {
+  //   databaseUrl.host = `${FLY_REGION}.${databaseUrl.host}`;
+  //   if (!isReadReplicaRegion) {
+  //     // 5433 is the read-replica port
+  //     databaseUrl.port = "5433";
+  //   }
+  // }
 
   console.log(`🔌 setting up prisma client to ${databaseUrl.host}`);
   // NOTE: during development if you change anything in this function, remember
